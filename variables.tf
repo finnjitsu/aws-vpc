ec6aee0a9595f14b977d45b1ebff136ef3281635
Jeremy Finn <jeremy.finn@gmail.com>
2020-03-19 00:22:57 -0400

    Add region variable and variable descriptions.

--- variables.tf
+++ variables.tf
@@ -1,3 +1,9 @@
 variable "cidr" {
   type = string
+  description = "CIDR block for the VPC."
+}
+
+variable "region" {
+  type        = string
+  description = "Region where the AWS resources will live."
 }

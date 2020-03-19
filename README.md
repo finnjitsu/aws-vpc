343e1c39acb35d39cc000f8a058ee575e7e8cafb
Jeremy Finn <jeremy.finn@gmail.com>
2020-03-19 14:36:10 -0400

    Add name tag. Enable dns. Improve readme doc.

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # aws-vpc
-Reusable infrastructure code to stand up a VPC in AWS.
+Reusable infrastructure code to stand up a VPC in AWS. Configures two subnets each for app tier, database tier, and web tier. 
--- variables.tf
+++ variables.tf
@@ -7,3 +7,8 @@ variable "region" {
   type        = string
   description = "Region where the AWS resources will live."
 }
+
+variable "vpc_name" {
+  type        = string
+  description = "Friendly name for the VPC."
+}

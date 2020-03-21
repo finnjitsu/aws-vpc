c01b3c5de78b3c6f6bf664382340199e69bcd9ff
Jeremy Finn <jeremy.finn@gmail.com>
2020-03-21 16:04:45 -0400

    Map public IP in web subnets.

--- vpc.tf
+++ vpc.tf
@@ -1,3 +1,9 @@
+/******************************************************************************
+*                                                                             *
+* VPC's and subnets                                                           *
+*                                                                             *
+******************************************************************************/
+
 resource "aws_vpc" "main" {
   cidr_block = var.vpc_cidr
   enable_dns_support   = true
@@ -11,7 +17,6 @@ resource "aws_subnet" "app_subnet_a" {
   vpc_id            = aws_vpc.main.id
   cidr_block        = var.app_subnet_a_cidr
   availability_zone = "${var.region}a"
-
   tags = {
     Name = "${var.stack_name}-app-subnet-a"
   }
@@ -21,7 +26,6 @@ resource "aws_subnet" "app_subnet_b" {
   vpc_id            = aws_vpc.main.id
   cidr_block        = var.app_subnet_b_cidr
   availability_zone = "${var.region}b"
-
   tags = {
     Name = "${var.stack_name}-app-subnet-b"
   }
@@ -31,7 +35,6 @@ resource "aws_subnet" "db_subnet_a" {
   vpc_id            = aws_vpc.main.id
   cidr_block        = var.db_subnet_a_cidr
   availability_zone = "${var.region}a"
-
   tags = {
     Name = "${var.stack_name}-db-subnet-a"
   }
@@ -41,28 +44,41 @@ resource "aws_subnet" "db_subnet_b" {
   vpc_id            = aws_vpc.main.id
   cidr_block        = var.db_subnet_b_cidr
   availability_zone = "${var.region}b"
-
   tags = {
     Name = "${var.stack_name}-db-subnet-b"
   }
 }
 
 resource "aws_subnet" "web_subnet_a" {
-  vpc_id            = aws_vpc.main.id
-  cidr_block        = var.web_subnet_a_cidr
-  availability_zone = "${var.region}a"
-
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = var.web_subnet_a_cidr
+  availability_zone       = "${var.region}a"
+  map_public_ip_on_launch = true
   tags = {
     Name = "${var.stack_name}-web-subnet-a"
   }
 }
 
 resource "aws_subnet" "web_subnet_b" {
-  vpc_id            = aws_vpc.main.id
-  cidr_block        = var.web_subnet_b_cidr
-  availability_zone = "${var.region}b"
-
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = var.web_subnet_b_cidr
+  availability_zone       = "${var.region}b"
+  map_public_ip_on_launch = true
   tags = {
     Name = "${var.stack_name}-web-subnet-b"
+  }
+}
+
+/******************************************************************************
+*                                                                             *
+* Internet connectivity                                                       *
+*                                                                             *
+******************************************************************************/
+
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "${var.stack_name}-main"
   }
 }

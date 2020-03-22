91941e1bf255c6c6833f315fddc87b2bd98191bf
Jeremy Finn <jeremy.finn@gmail.com>
2020-03-21 23:17:39 -0400

    Fix routes.

--- vpc.tf
+++ vpc.tf
@@ -84,13 +84,15 @@ resource "aws_internet_gateway" "main" {
 
 resource "aws_route_table" "web" {
   vpc_id = aws_vpc.main.id
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.main.id
-  }
   tags = {
     Name = "${var.stack_name}-main"
   }
+}
+
+resource "aws_route" "web_default" {
+  route_table_id          = aws_route_table.web.id
+  destination_cidr_block  = "0.0.0.0/0"
+  gateway_id              = aws_internet_gateway.main.id
 }
 
 resource "aws_route_table_association" "web_a" {
@@ -100,10 +102,5 @@ resource "aws_route_table_association" "web_a" {
 
 resource "aws_route_table_association" "web_b" {
   subnet_id      = aws_subnet.web_subnet_b.id
-  route_table_id = aws_route_table.web.id
-}
-
-resource "aws_route_table_association" "gateway" {
-  gateway_id     = aws_internet_gateway.main.id
   route_table_id = aws_route_table.web.id
 }
